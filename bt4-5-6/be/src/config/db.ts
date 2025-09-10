8fcc01b0d7f69de05ac933153421b377d89ebab7
DucSang404 <ndsang404@gmail.com>
2025-09-10 09:21:53 +0700

    06-1

--- bt4-5-6/be/src/config/db.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const connectDB = async () => {
-  const uri = process.env.MONGODB_URI!;
-  if (!uri) throw new Error('MONGODB_URI missing');
-  await mongoose.connect(uri);
-  console.log('MongoDB connected');
-};
